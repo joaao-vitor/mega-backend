d294ca142a7a3eca97874dbd395ef3f32e48a4d3
João Vitor <jvitor.arrudal@gmail.com>
2023-11-29 08:35:57 -0300

    fixes

--- controllers/userProgressController.js
+++ controllers/userProgressController.js
@@ -8,11 +8,11 @@ const deleteProgress = async (req, res) => {
 }
 const addProgress = async (req, res) => {
 
-    
+
     const { usuarioid } = req.user
     const { jogoid, progressoid } = req.body
-    
-    await pool.query(queries.deleteProgress, [usuarioid])
+
+    await pool.query(queries.deleteProgress, [usuarioid, jogoid])
     const results = await pool.query(queries.addProgress, [usuarioid, progressoid, jogoid])
     res.status(200).json({ msg: "Ação realizada com sucesso!" })
 }
